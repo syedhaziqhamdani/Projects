66faee7108062c65f0b2bf9b0fed020591cbe96a
Syed Haziq Hamdani <syedhaziqhamdani@hotmail.com>
2014-12-15 01:54:45 +0500

    :+1:

--- ArtificialIntelligenceProject/src/Main.java
+++ ArtificialIntelligenceProject/src/Main.java
@@ -8,13 +8,17 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Main extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField[][] grid = new JTextField[6][7];
+	private static JPanel contentPane;
+	private static JTextField[][] grid = new JTextField[6][7];
 //	private JTextField b00;
 //	private JTextField b01;
 //	private JTextField b02;
@@ -78,7 +82,7 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
-		grid[0][0] = new JTextField();
+		grid[0][0] = new JTextField("a");
 		grid[0][1] = new JTextField();
 		grid[0][2] = new JTextField();
 		grid[0][3] = new JTextField();
@@ -129,6 +133,12 @@ public class Main extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton columnOneButton = new JButton("Drop");
+		columnOneButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+//				JOptionPane.showConfirmDialog(contentPane, "Clicked");
+				humanMove(0);
+			}
+		});
 		columnOneButton.setBounds(10, 11, 89, 23);
 		contentPane.add(columnOneButton);
 		
@@ -156,273 +166,290 @@ public class Main extends JFrame {
 		columnSevenButton.setBounds(604, 11, 89, 23);
 		contentPane.add(columnSevenButton);
 				
-		grid[0][0] = new JTextField();
+//		grid[0][0] = new JTextField();
 		grid[0][0].setEnabled(false);
 		grid[0][0].setBounds(10, 45, 86, 20);
 		contentPane.add(grid[0][0]);
 		grid[0][0].setColumns(10);
 		
-		grid[0][1] = new JTextField();
+//		grid[0][1] = new JTextField();
 		grid[0][1].setEnabled(false);
 		grid[0][1].setColumns(10);
 		grid[0][1].setBounds(109, 45, 86, 20);
 		contentPane.add(grid[0][1]);
 		
-		grid[0][2] = new JTextField();
+//		grid[0][2] = new JTextField();
 		grid[0][2].setEnabled(false);
 		grid[0][2].setColumns(10);
 		grid[0][2].setBounds(211, 45, 86, 20);
 		contentPane.add(grid[0][2]);
 		
-		grid[0][3] = new JTextField();
+//		grid[0][3] = new JTextField();
 		grid[0][3].setEnabled(false);
 		grid[0][3].setColumns(10);
 		grid[0][3].setBounds(310, 45, 86, 20);
 		contentPane.add(grid[0][3]);
 		
-		grid[0][4] = new JTextField();
+//		grid[0][4] = new JTextField();
 		grid[0][4].setEnabled(false);
 		grid[0][4].setColumns(10);
 		grid[0][4].setBounds(406, 45, 86, 20);
 		contentPane.add(grid[0][4]);
 		
-		grid[0][5] = new JTextField();
+//		grid[0][5] = new JTextField();
 		grid[0][5].setEnabled(false);
 		grid[0][5].setColumns(10);
 		grid[0][5].setBounds(508, 45, 86, 20);
 		contentPane.add(grid[0][5]);
 		
-		grid[0][6] = new JTextField();
+//		grid[0][6] = new JTextField(" ");
 		grid[0][6].setEnabled(false);
 		grid[0][6].setColumns(10);
 		grid[0][6].setBounds(607, 45, 86, 20);
 		contentPane.add(grid[0][6]);
 		
-		grid[1][0] = new JTextField();
+//		grid[1][0] = new JTextField();
 		grid[1][0].setEnabled(false);
 		grid[1][0].setColumns(10);
 		grid[1][0].setBounds(10, 76, 86, 20);
 		contentPane.add(grid[1][0]);
 		
-		grid[1][1] = new JTextField();
+//		grid[1][1] = new JTextField();
 		grid[1][1].setEnabled(false);
 		grid[1][1].setColumns(10);
 		grid[1][1].setBounds(109, 76, 86, 20);
 		contentPane.add(grid[1][1]);
 		
-		grid[1][2] = new JTextField();
+//		grid[1][2] = new JTextField();
 		grid[1][2].setEnabled(false);
 		grid[1][2].setColumns(10);
 		grid[1][2].setBounds(211, 76, 86, 20);
 		contentPane.add(grid[1][2]);
 		
-		grid[1][3] = new JTextField();
+//		grid[1][3] = new JTextField();
 		grid[1][3].setEnabled(false);
 		grid[1][3].setColumns(10);
 		grid[1][3].setBounds(310, 76, 86, 20);
 		contentPane.add(grid[1][3]);
 		
-		grid[1][4] = new JTextField();
+//		grid[1][4] = new JTextField();
 		grid[1][4].setEnabled(false);
 		grid[1][4].setColumns(10);
 		grid[1][4].setBounds(406, 76, 86, 20);
 		contentPane.add(grid[1][4]);
 		
-		grid[1][5] = new JTextField();
+//		grid[1][5] = new JTextField();
 		grid[1][5].setEnabled(false);
 		grid[1][5].setColumns(10);
 		grid[1][5].setBounds(508, 76, 86, 20);
 		contentPane.add(grid[1][5]);
 		
-		grid[1][6] = new JTextField();
+//		grid[1][6] = new JTextField();
 		grid[1][6].setEnabled(false);
 		grid[1][6].setColumns(10);
 		grid[1][6].setBounds(607, 76, 86, 20);
 		contentPane.add(grid[1][6]);
 
-		grid[2][0] = new JTextField();
+//		grid[2][0] = new JTextField();
 		grid[2][0].setEnabled(false);
 		grid[2][0].setColumns(10);
 		grid[2][0].setBounds(10, 107, 86, 20);
 		contentPane.add(grid[2][0]);
 		
-		grid[2][1] = new JTextField();
+//		grid[2][1] = new JTextField();
 		grid[2][1].setEnabled(false);
 		grid[2][1].setColumns(10);
 		grid[2][1].setBounds(109, 107, 86, 20);
 		contentPane.add(grid[2][1]);
 		
-		grid[2][2] = new JTextField();
+//		grid[2][2] = new JTextField();
 		grid[2][2].setEnabled(false);
 		grid[2][2].setColumns(10);
 		grid[2][2].setBounds(211, 107, 86, 20);
 		contentPane.add(grid[2][2]);
 		
-		grid[2][3] = new JTextField();
+//		grid[2][3] = new JTextField();
 		grid[2][3].setEnabled(false);
 		grid[2][3].setColumns(10);
 		grid[2][3].setBounds(310, 107, 86, 20);
 		contentPane.add(grid[2][3]);
 		
-		grid[2][4] = new JTextField();
+//		grid[2][4] = new JTextField();
 		grid[2][4].setEnabled(false);
 		grid[2][4].setColumns(10);
 		grid[2][4].setBounds(406, 107, 86, 20);
 		contentPane.add(grid[2][4]);
 		
-		grid[2][5] = new JTextField();
+//		grid[2][5] = new JTextField();
 		grid[2][5].setEnabled(false);
 		grid[2][5].setColumns(10);
 		grid[2][5].setBounds(508, 107, 86, 20);
 		contentPane.add(grid[2][5]);
 		
-		grid[2][6] = new JTextField();
+//		grid[2][6] = new JTextField();
 		grid[2][6].setEnabled(false);
 		grid[2][6].setColumns(10);
 		grid[2][6].setBounds(607, 107, 86, 20);
 		contentPane.add(grid[2][6]);
 		
-		grid[3][0] = new JTextField();
+//		grid[3][0] = new JTextField();
 		grid[3][0].setEnabled(false);
 		grid[3][0].setColumns(10);
 		grid[3][0].setBounds(10, 138, 86, 20);
 		contentPane.add(grid[3][0]);
 		
-		grid[3][1] = new JTextField();
+//		grid[3][1] = new JTextField();
 		grid[3][1].setEnabled(false);
 		grid[3][1].setColumns(10);
 		grid[3][1].setBounds(109, 138, 86, 20);
 		contentPane.add(grid[3][1]);
 		
-		grid[3][2] = new JTextField();
+//		grid[3][2] = new JTextField();
 		grid[3][2].setEnabled(false);
 		grid[3][2].setColumns(10);
 		grid[3][2].setBounds(211, 138, 86, 20);
 		contentPane.add(grid[3][2]);
 		
-		grid[3][3] = new JTextField();
+//		grid[3][3] = new JTextField();
 		grid[3][3].setEnabled(false);
 		grid[3][3].setColumns(10);
 		grid[3][3].setBounds(310, 138, 86, 20);
 		contentPane.add(grid[3][3]);
 		
-		grid[3][4] = new JTextField();
+//		grid[3][4] = new JTextField();
 		grid[3][4].setEnabled(false);
 		grid[3][4].setColumns(10);
 		grid[3][4].setBounds(406, 138, 86, 20);
 		contentPane.add(grid[3][4]);
 		
-		grid[3][5] = new JTextField();
+//		grid[3][5] = new JTextField();
 		grid[3][5].setEnabled(false);
 		grid[3][5].setColumns(10);
 		grid[3][5].setBounds(508, 138, 86, 20);
 		contentPane.add(grid[3][5]);
 		
-		grid[3][6] = new JTextField();
+//		grid[3][6] = new JTextField();
 		grid[3][6].setEnabled(false);
 		grid[3][6].setColumns(10);
 		grid[3][6].setBounds(607, 138, 86, 20);
 		contentPane.add(grid[3][6]);
 		
-		grid[4][0] = new JTextField();
+//		grid[4][0] = new JTextField();
 		grid[4][0].setEnabled(false);
 		grid[4][0].setColumns(10);
 		grid[4][0].setBounds(10, 169, 86, 20);
 		contentPane.add(grid[4][0]);
 		
-		grid[4][1] = new JTextField();
+//		grid[4][1] = new JTextField();
 		grid[4][1].setEnabled(false);
 		grid[4][1].setColumns(10);
 		grid[4][1].setBounds(109, 169, 86, 20);
 		contentPane.add(grid[4][1]);
 		
-		grid[4][2] = new JTextField();
+//		grid[4][2] = new JTextField();
 		grid[4][2].setEnabled(false);
 		grid[4][2].setColumns(10);
 		grid[4][2].setBounds(211, 169, 86, 20);
 		contentPane.add(grid[4][2]);
 		
-		grid[4][3] = new JTextField();
+//		grid[4][3] = new JTextField();
 		grid[4][3].setEnabled(false);
 		grid[4][3].setColumns(10);
 		grid[4][3].setBounds(310, 169, 86, 20);
 		contentPane.add(grid[4][3]);
 		
-		grid[4][4] = new JTextField();
+//		grid[4][4] = new JTextField();
 		grid[4][4].setEnabled(false);
 		grid[4][4].setColumns(10);
 		grid[4][4].setBounds(406, 169, 86, 20);
 		contentPane.add(grid[4][4]);
 		
-		grid[4][5] = new JTextField();
+//		grid[4][5] = new JTextField();
 		grid[4][5].setEnabled(false);
 		grid[4][5].setColumns(10);
 		grid[4][5].setBounds(508, 169, 86, 20);
 		contentPane.add(grid[4][5]);
 		
-		grid[4][6] = new JTextField();
+//		grid[4][6] = new JTextField();
 		grid[4][6].setEnabled(false);
 		grid[4][6].setColumns(10);
 		grid[4][6].setBounds(607, 169, 86, 20);
 		contentPane.add(grid[4][6]);
 		
 		
-		grid[5][0] = new JTextField();
+//		grid[5][0] = new JTextField();
 		grid[5][0].setEnabled(false);
 		grid[5][0].setColumns(10);
 		grid[5][0].setBounds(10, 200, 86, 20);
 		contentPane.add(grid[5][0]);
 		
-		grid[5][1] = new JTextField();
+//		grid[5][1] = new JTextField();
 		grid[5][1].setEnabled(false);
 		grid[5][1].setColumns(10);
 		grid[5][1].setBounds(109, 200, 86, 20);
 		contentPane.add(grid[5][1]);
 		
-		grid[5][2] = new JTextField();
+//		grid[5][2] = new JTextField();
 		grid[5][2].setEnabled(false);
 		grid[5][2].setColumns(10);
 		grid[5][2].setBounds(211, 200, 86, 20);
 		contentPane.add(grid[5][2]);
 		
-		grid[5][3] = new JTextField();
+//		grid[5][3] = new JTextField();
 		grid[5][3].setEnabled(false);
 		grid[5][3].setColumns(10);
 		grid[5][3].setBounds(310, 200, 86, 20);
 		contentPane.add(grid[5][3]);
 		
-		grid[5][4] = new JTextField();
+//		grid[5][4] = new JTextField();
 		grid[5][4].setEnabled(false);
 		grid[5][4].setColumns(10);
 		grid[5][4].setBounds(406, 200, 86, 20);
 		contentPane.add(grid[5][4]);
 		
-		grid[5][5] = new JTextField();
+//		grid[5][5] = new JTextField();
 		grid[5][5].setEnabled(false);
 		grid[5][5].setColumns(10);
 		grid[5][5].setBounds(508, 200, 86, 20);
 		contentPane.add(grid[5][5]);
 		
-		grid[5][6] = new JTextField();
+//		grid[5][6] = new JTextField();
 		grid[5][6].setEnabled(false);
 		grid[5][6].setColumns(10);
 		grid[5][6].setBounds(607, 200, 86, 20);
 		contentPane.add(grid[5][6]);
 		
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid.length; j++) {
-				if(j==6){
-					if (grid[i][6].toString().equals("")) {
-						JOptionPane.showConfirmDialog(contentPane, grid[i][6].toString());
-					}
-
-				}
-				grid[i][j].setText("Test.");
-			}
-		}
+//		Error Checking Code
+//		for (int i = 0; i < 6; i++) {
+//			for (int j = 0; j < 7; j++) {
+//				grid[i][j].setText("Test.");
+//			}
+//		}
 	}
 	static void humanMove(int columnNumber){
-		for (int i = 0; i < 6; i++) {
+		JOptionPane.showConfirmDialog(contentPane, "Inside Method.");
+		for (int i = 0; i <= 5; i++) {
+			
+			if (grid[i][columnNumber].getText().isEmpty()) {
+				if (grid.length-1 == i) {
+					grid[i][columnNumber].setBackground(Color.BLUE);
+				}
+			}
+			
 		}
+		
+		
+//		while (grid[row][columnNumber].toString().equals("")) {
+//			
+//			row++;
+//		}
+//		
+//		for (int i = 0; i < 6; i++) {
+//			JOptionPane.showConfirmDialog(contentPane, "Inside Meto1");
+//			if (grid[i][columnNumber].toString().equals("")) {
+//				JOptionPane.showConfirmDialog(contentPane, "Inside Meto2");
+//				grid[i][columnNumber].setText("Human.");
+//			}
+//		}
 	}
 }
